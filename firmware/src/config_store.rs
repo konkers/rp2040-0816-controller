2a24435f963a68b42f3b8edd8408a641c8bfa601
Erik Gilling <konkers@konkers.net>
2023-12-01 18:55:41 -0800

    Add saving of feeder config data.

---
+++ firmware/src/config_store.rs
@@ -0,0 +1,128 @@
+use core::ops::Range;
+
+use embedded_storage::nor_flash::NorFlash;
+use pnpfeeder::{ConfigStore, Error, FeederConfig, Value};
+use sequential_storage::map::{fetch_item, store_item, StorageItem};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+enum ConfigKey {
+    FeederConfigV0(usize),
+}
+
+enum ConfigValue {
+    FeederConfigV0(FeederConfig),
+}
+
+struct ConfigStorageItem {
+    key: ConfigKey,
+    value: ConfigValue,
+}
+
+impl ConfigStorageItem {
+    // Key = 2 u32s, FeederConfig =
+    const KEY_WORDS: usize = 2;
+    const FEEDER_WORDS: usize = 8;
+    const PADDING_WORDS: usize = 0;
+    const BYTES_PER_WORD: usize = 5;
+    const BUFFER_SIZE: usize =
+        (Self::KEY_WORDS + Self::FEEDER_WORDS + Self::PADDING_WORDS) * Self::BYTES_PER_WORD;
+
+    fn new_config(index: usize, config: FeederConfig) -> Self {
+        Self {
+            key: ConfigKey::FeederConfigV0(index),
+            value: ConfigValue::FeederConfigV0(config),
+        }
+    }
+}
+
+impl StorageItem for ConfigStorageItem {
+    type Key = ConfigKey;
+    type Error = Error;
+
+    fn serialize_into(&self, buffer: &mut [u8]) -> Result<usize, Self::Error> {
+        let key_buf = postcard::to_slice(&self.key, buffer).map_err(|_| Error::ConfigSetError)?;
+        let key_len = key_buf.len();
+        let value_buf = &mut buffer[key_len..];
+        let value_buf = match &self.value {
+            ConfigValue::FeederConfigV0(config) => {
+                postcard::to_slice(&config, value_buf).map_err(|_| Error::ConfigSetError)?
+            }
+        };
+
+        Ok(key_len + value_buf.len())
+    }
+
+    fn deserialize_from(buffer: &[u8]) -> Result<Self, Self::Error>
+    where
+        Self: Sized,
+    {
+        let (key, value_buf) =
+            postcard::take_from_bytes(buffer).map_err(|_| Error::ConfigSetError)?;
+        let value = match key {
+            ConfigKey::FeederConfigV0(_) => {
+                let config = postcard::from_bytes(value_buf).map_err(|_| Error::ConfigSetError)?;
+                ConfigValue::FeederConfigV0(config)
+            }
+        };
+
+        Ok(Self { key, value })
+    }
+
+    fn key(&self) -> Self::Key {
+        self.key.clone()
+    }
+}
+
+pub struct FlashConfigStore<Flash: NorFlash> {
+    flash: Flash,
+    range: Range<u32>,
+}
+
+impl<Flash: NorFlash> FlashConfigStore<Flash> {
+    pub fn new(flash: Flash, range: Range<u32>) -> Self {
+        Self { flash, range }
+    }
+
+    fn default_config() -> FeederConfig {
+        FeederConfig {
+            advanced_angle: Value::from_num(135.0),
+            half_advanced_angle: Value::from_num(107.5),
+            retract_angle: Value::from_num(80),
+            feed_length: Value::from_num(2.0),
+            settle_time: 300,
+            pwm_0: Value::from_num(490.2),
+            pwm_180: Value::from_num(980.4),
+            ignore_feeback_pin: false,
+        }
+    }
+}
+
+impl<Flash: NorFlash> ConfigStore for FlashConfigStore<Flash> {
+    fn get(&mut self, index: usize) -> pnpfeeder::Result<FeederConfig> {
+        let mut buf = [0u8; ConfigStorageItem::BUFFER_SIZE];
+        let range = self.range.clone();
+        // Too much extraneous error handling here.  We should be able to clean this up.
+        let item: Option<ConfigStorageItem> = fetch_item(
+            &mut self.flash,
+            range,
+            &mut buf,
+            ConfigKey::FeederConfigV0(index),
+        )
+        .map_err(|_| Error::ConfigGetError)?;
+
+        match item
+            .map(|item| item.value)
+            .unwrap_or(ConfigValue::FeederConfigV0(Self::default_config()))
+        {
+            ConfigValue::FeederConfigV0(feeder) => Ok(feeder),
+        }
+    }
+
+    fn set(&mut self, index: usize, config: &FeederConfig) -> pnpfeeder::Result<()> {
+        let mut buf = [0u8; ConfigStorageItem::BUFFER_SIZE];
+        let range = self.range.clone();
+        let item = ConfigStorageItem::new_config(index, config.clone());
+        store_item(&mut self.flash, range, &mut buf, item).map_err(|_| Error::ConfigSetError)
+    }
+}
--- firmware/src/lib.rs
+++ firmware/src/lib.rs
@@ -4,6 +4,7 @@
 // This is used for `utf8_char_width`.
 #![feature(str_internals)]
 
+pub mod config_store;
 pub mod gpio_input;
 pub mod pwm_servo;
 pub mod usb;
